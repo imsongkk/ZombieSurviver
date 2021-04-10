7e8975d1a8d0b3add19225ebd1497e81eea7b2cd
Imsung_You <isyou7487@naver.com>
2021-04-10 18:16:26 +0900

    added Network preset

--- Assets/Scripts/AmmoPack.cs
+++ Assets/Scripts/AmmoPack.cs
@@ -1,7 +1,8 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using UnityEngine;
 
 // 총알을 충전하는 아이템
-public class AmmoPack : MonoBehaviour, IItem {
+public class AmmoPack : MonoBehaviourPun, IItem {
     public int ammo = 30; // 충전할 총알 수
 
     public void Use(GameObject target) {
@@ -11,11 +12,11 @@ public class AmmoPack : MonoBehaviour, IItem {
         // PlayerShooter 컴포넌트가 있으며, 총 오브젝트가 존재하면
         if (playerShooter != null && playerShooter.gun != null)
         {
-            // 총의 남은 탄환 수를 ammo 만큼 더한다
-            playerShooter.gun.ammoRemain += ammo;
+            // 총의 남은 탄환 수를 ammo 만큼 더하기, 모든 클라이언트에서 실행
+            playerShooter.gun.photonView.RPC("AddAmmo", RpcTarget.All, ammo);
         }
 
-        // 사용되었으므로, 자신을 파괴
-        Destroy(gameObject);
+        // 모든 클라이언트에서의 자신을 파괴
+        PhotonNetwork.Destroy(gameObject);
     }
 }
